518676633bdb22da13a830358afce93d393bfd2c
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-07-16 06:25:00 -0700

    Refactored robot class and added choice to select random weapon from list of weapons.

--- robot.py
+++ robot.py
@@ -1,8 +1,22 @@
+from weapon import Weapon
+import random
+
 class Robot:
-    def __init__(self, name, health, weapon):
+    def __init__(self, name, health):
         self.name = name
         self.health = health #will be int
-        self.weapon = weapon
+        self.weapons = self.weapon_populate()
+        self.weapon_choice = self.select_weapons()
         
-    def attack(self, dinosaur):
-        self.dinosaur = dinosaur
+    def attack(self, Dinosaur):
+        if (Dinosaur.health != 0):
+            Dinosaur.health = Dinosaur.health - self.weapon.attack_power
+    
+    def select_weapons(self):
+        return random.choice(self.weapons)
+    
+    def weapon_populate(self):
+        chainsaw = Weapon('', 25)
+        raygun = Weapon('Raygun', 25)
+        astroid_launcher = Weapon('AstroidLauncher', 25)
+        return [chainsaw, raygun, astroid_launcher]
